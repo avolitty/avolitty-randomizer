a342020c8d257bb98e5add72e5ebcccb352a3487
William Parsons <avolitty@gmail.com>
2022-12-25 19:06:50 +0000

    Populating int array with seed values from tmpnam().

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	char _0[16];
+	int _1[6] = {0, 0, 0, 0, 0, 0};
+	int _2 = 15;
+	tmpnam(_0);
+
+	while (_2-- != 9) {
+		_1[_2 - 9] = _0[_2];
+	}
+
+	printf("%i", _1[0]);
+	// ..
+	return 0;
+}
+
