25e783fc8da1882c6d6d48d421c4ca40e7c538dd
William Parsons <avolitty@gmail.com>
2023-01-06 13:21:55 -0800

    Adding entropy with bit shifting for tmpnam result bytes.

--- src/avolitty-randomizer.c
+++ src/avolitty-randomizer.c
@@ -5,7 +5,8 @@ void AvolittyRandomizer(signed short int *a) {
 	signed short int c = 0;
 	signed short int d = 0;
 	char *e = 0;
-	char *f = tmpnam(e);
+	char *f;
+	f = tmpnam(e);
 	f[0] = 47;
 
 	while (f[c] != 0) {
@@ -13,7 +14,7 @@ void AvolittyRandomizer(signed short int *a) {
 	}
 
 	while (f[c--] != 47) {
-		b[d] = (b[d] + (signed short int) f[c]) & 8191;
+		b[d] = (b[d] + (signed short int) f[c]) & 2047;
 		d = d + 1 & 7;
 	}
 
@@ -21,26 +22,26 @@ void AvolittyRandomizer(signed short int *a) {
 	d = 0;
 
 	while (c-- != 0) {
-		b[d] = b[c] + b[d] & 8191;
-		b[c] = b[c] + b[d++] & 8191;
+		b[c] += b[d] + 2;
+		b[d++] = (b[c] >> 1) + b[c] & 2047;
 	}
 
-	c = (b[0] + b[2] + b[4] + b[6] & 3) * 10000;
-	c += ((b[1] + b[3] & 1023) + c & 15) + ((b[5] + b[7] & 1023) + c & 15);
+	c = (b[0] + b[2] + b[4] & 3) * 10000;
+	c += (b[1] + b[3] + b[5] & 15) + (b[2] + b[4] + b[6] & 15);
 
 	if (c < 30000) {
 		c += (b[0] & 1) + (b[1] & 1) + (b[2] & 7) * 1000;
 		c += (b[3] & 1) + (b[4] & 1) + (b[5] & 7) * 100;
-		c += (b[6] & 1) + (b[7] & 1) + (b[0] & 7) * 10;
-		c += (b[1] & 1) + (b[2] & 1) + (b[3] & 7);
+		c += (b[6] & 1) + (b[0] & 1) + (b[1] & 7) * 10;
+		c += (b[2] & 1) + (b[5] & 1) + (b[3] & 7);
 	} else {
-		c += ((b[0] & 1) + (b[1] & 1)) * 1000;
-		c += (b[2] + b[3] & 7) * 100;
-		c += (b[4] + b[5] & 3) * 10;
-		c += b[6] + b[7] & 7;
+		c += (b[0] & 1) + (b[1] & 1) * 1000;
+		c += (b[2] & 7) * 100;
+		c += (b[3] & 3) * 10;
+		c += b[4] & 7;
 	}
 
-	if ((b[0] & 1) == 0) {
+	if ((b[7] & 1) == 0) {
 		c = ~c;
 	}
 
