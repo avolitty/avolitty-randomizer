5b4dc90553aaeeb7f0619f1cf0e8c614334865e2
William Parsons <avolitty@gmail.com>
2023-01-26 12:38:29 -0800

    Initializing more variables with correct casted data types.

--- src/avolitty-randomizer.c
+++ src/avolitty-randomizer.c
@@ -7,59 +7,70 @@ unsigned short int AvolittyRandomizer(unsigned char a) {
 	unsigned short int e;
 	unsigned short int f;
 	unsigned short int g;
-	char h;
-	char i;
-	char *j;
-	char *k;
-	unsigned char l;
+	unsigned short int h;
+	unsigned short int i;
+	unsigned short int j;
+	unsigned short int k;
+	char l;
+	char m;
+	char n;
+	char *o;
+	char *p;
+	unsigned char q;
 	d = ((unsigned short int) 2047U);
 	e = ((unsigned short int) 127U);
-	f = ((unsigned short int) 0U);
-	g = ((unsigned short int) 0U);
-	h = ((char) 0);
-	i = ((char) 0);
-	l = ((unsigned char) 0U);
+	f = ((unsigned short int) 7U);
+	g = ((unsigned short int) 2U);
+	h = ((unsigned short int) 1U);
+	i = ((unsigned short int) 0U);
+	j = ((unsigned short int) 0U);
+	k = ((unsigned short int) 0U);
+	l = ((char) 47);
+	m = ((char) 0);
+	n = ((char) 0);
+	q = ((unsigned char) 0U);
 
-	while (a != l) {
-		j = ((char) 0);
-		k = tmpnam(j);
-		k[((unsigned short int) 0U)] = ((char) 47);
+	while (a != q) {
+		o = ((char) 0);
+		p = tmpnam(o);
+		p[f] = l;
 
-		while (h != k[g]) {
-			b[(g & ((unsigned short int) 7U))] = ((unsigned short int) 0U);
-			g++;
+		while (m != p[j]) {
+			k = (j & f);
+			b[k] = j++;
 		}
 
-		h = ((char) 47);
+		k = i;
+		m = l;
 
-		while (h != k[g]) {
-			c[f] = b[f];
+		while (m != p[j]) {
+			c[f] = b[k];
 
-			if (i > k[g]) {
-				c[f] += e;
-				i--;
-				k[g] *= i;
-				i++;
+			if (n > p[j]) {
+				c[k] += e;
+				n--;
+				p[j] *= n++;
 			}
 
-			b[f] = ((c[f] + ((unsigned short int) k[g])) & d);
-			f++;
-			f &= 7U;
-			g--;
+			b[k] = (c[k] + ((unsigned short int) p[j]));
+			b[k++] &= d;
+			k &= f;
+			j--;
 		}
 
-		f = ((unsigned short int) 8U);
-		g = 0U;
+		f++;
+		j = f--;
+		k = i;
 
-		while (f != 0U) {
-			f--;
-			b[f] += (b[g] + 2U);
-			b[g] = (((b[f] >> 1U) + c[g]) & d);
-			g++;
+		while (i != j) {
+			j--;
+			b[j] += (b[k] + g);
+			b[k] = ((b[j] >> h) + c[k]);
+			b[k++] &= d;
 		}
 
 		a--;
-		g = 0U;
+		k = i;
 	}
 
 	d = ((((b[0U] + c[2U] + b[4U]) & 3U) * 10000U) + ((b[1U] + b[3U] + b[5U]) & 15U) + ((c[2U] + b[4U] + c[6U]) & 15U));
