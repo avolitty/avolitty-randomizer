b67a4f25a1d9767d7de73361c3af5d575e990bc9
William Parsons <avolitty@gmail.com>
2023-01-29 21:51:55 -0800

    Adding negative operand and data types in compliance with ANSI C.

--- src/avolitty-randomizer.c
+++ src/avolitty-randomizer.c
@@ -18,11 +18,10 @@ unsigned short int AvolittyRandomizer(unsigned char a) {
 	char p;
 	char *q;
 	char *r;
-	char *s;
-	unsigned char t;
+	unsigned char s;
 	d = ((unsigned short int) 2047U);
 	e = ((unsigned short int) 127U);
-	f = ((unsigned short int) 15U);
+	f = ((unsigned short int) 8U);
 	g = ((unsigned short int) 7U);
 	h = ((unsigned short int) 6U);
 	i = ((unsigned short int) 2U);
@@ -33,46 +32,43 @@ unsigned short int AvolittyRandomizer(unsigned char a) {
 	n = ((char) 47);
 	o = ((char) 0);
 	p = ((char) 0);
-	q = ((char) 0);
-	t = ((unsigned char) 0U);
+	q = ((char *) 0);
+	s = ((unsigned char) 0U);
 
-	while (a != t) {
-		r = q;
-		s = tmpnam(r);
-		s[g] = n;
+	while (a != s) {
+		r = tmpnam(q);
+		r[g] = n;
 
-		while (o != s[l]) {
+		while (o != r[l]) {
 			m = (l & g);
-			b[m] = l++;
+			b[m] = l;
+			l++;
 		}
 
 		m = k;
 		o = n;
 
-		while (o != s[l]) {
+		while (o != r[l]) {
 			c[g] = b[m];
 
-			if (p > s[l]) {
+			if (p > r[l]) {
 				c[m] += e;
-				p--;
-				s[l] *= p++;
+				r[l] = (-r[l]);
 			}
 
-			b[m] = ((c[m] + ((unsigned short int) s[l])) & d);
+			b[m] = ((c[m] + ((unsigned short int) r[l])) & d);
 			m++;
 			m &= g;
 			l--;
 		}
 
-		g++;
-		l = g--;
+		l = f;
 		m = k;
 
 		while (k != l) {
 			l--;
 			b[l] += (b[m] + i);
-			b[m] = ((b[l] >> j) + c[m]);
-			b[m] &= d;
+			b[m] = (((b[l] >> j) + c[m]) & d);
 			m++;
 		}
 
@@ -80,16 +76,17 @@ unsigned short int AvolittyRandomizer(unsigned char a) {
 		m = k;
 	}
 
-	e = ((signed short int) 5U);
-	l = ((signed short int) 4U);
-	m = ((signed short int) 3U);
-	d = (((((b[k] + c[i]) + b[l]) & m) * ((unsigned short int) 10000U)) + (((b[j] + b[m]) + b[e]) & f) + (((c[i] + b[l]) + c[h]) & f));
+	e = ((unsigned short int) 15U);
+	f = ((unsigned short int) 5U);
+	l = ((unsigned short int) 4U);
+	m = ((unsigned short int) 3U);
+	d = (((((b[k] + c[i]) + b[l]) & m) * ((unsigned short int) 10000U)) + (((b[j] + b[m]) + b[f]) & e) + (((c[i] + b[l]) + c[h]) & e));
 
 	if (d < ((unsigned short int) 30000U)) {
 		d += ((((b[k] & j) + ((b[j] + c[j]) & j)) + (b[i] & g)) * ((unsigned short int) 1000U));
-		d += (((((b[m] + c[m]) & j) + (b[l] & j)) + ((b[e] + c[e]) & g)) * ((unsigned short int) 100U));
+		d += (((((b[m] + c[m]) & j) + (b[l] & j)) + ((b[f] + c[e]) & g)) * ((unsigned short int) 100U));
 		d += ((((b[h] & j) + (c[k] & j)) + (b[j] & g)) * ((unsigned short int) 10U));
-		d += ((((b[i] + c[i]) & j) + (b[e] & j)) + ((b[i] + c[m]) & g));
+		d += ((((b[i] + c[i]) & j) + (b[f] & j)) + ((b[i] + c[m]) & g));
 	} else {
 		d += (((b[k] & j) + ((b[j] + c[j]) & j)) * ((unsigned short int) 1000U));
 		d += ((b[i] & g) * ((unsigned short int) 100U));
